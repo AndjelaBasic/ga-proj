38db463b2999c6d1f06bfe5e894af78cc8843ac2
AndjelaBasic <47024700+AndjelaBasic@users.noreply.github.com>
2024-09-01 10:31:44 +0200

    Adding core files

---
+++ animacijanit.cpp
@@ -0,0 +1,18 @@
+#include <GL/glew.h>
+
+#include "animacijanit.h"
+#include "algoritambaza.h"
+
+AnimacijaNit::AnimacijaNit(AlgoritamBaza *pAlgoritam)
+    :QThread{}, _pAlgoritam(pAlgoritam)
+{}
+
+void AnimacijaNit::run()
+{
+    if (!_pAlgoritam) return;
+
+    if (_pAlgoritam->naivni())
+        _pAlgoritam->pokreniNaivniAlgoritam();
+    else
+        _pAlgoritam->pokreniAlgoritam();
+}
